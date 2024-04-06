82e3bc7ba6ee060f15a583d71b78102d673ce2e9
Sudhanshu-code <mail.sudhanshug@gmail.com>
2024-04-06 16:50:55 +0530

    /login added

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+app.get("/login", (req, res) => {
+  res.send("Please Login to start with");
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`);
